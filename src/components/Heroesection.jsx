f149ee93e13d2baf6c4ffaa0ff8fa0df2fef4641
Winnie Chan <wchan0806@gmail.com>
2026-01-10 01:25:00 -0500

    Finished the heroesection

--- src/components/Heroesection.jsx
+++ src/components/Heroesection.jsx
@@ -1,9 +1,33 @@
-function Herosection () {
-    return (
-        <div>
+import TeamPhoto from "../assets/TeamPhoto.svg";
+import Mascot from "../assets/Mascot.svg";
+import { Sprout } from "lucide-react";
+import "./Heroesection.css";
 
-        </div>
-    )
+function Herosection() {
+  return (
+    <div className="heroesection centerFlex">
+      <div className="description">
+        <h2 style={{ marginTop: "10px" }}>
+          <em>Computational Sustainability Club</em>
+        </h2>
+        <h1>
+          ADVOCATING <Sprout size={50} /> FOR{" "}
+          <span>
+            <em>GREEN TECH</em>
+          </span>
+        </h1>
+        <p>
+          We are a student-led team <span>empowering</span> Cornellians to{" "}
+          <span>leverage</span> technology and data to{" "}
+          <span>solve environmental</span> challenges.
+        </p>
+      </div>
+      <div className="teamPhoto">
+        <img src={TeamPhoto} alt="" />
+      </div>
+      <img src={Mascot} alt="A drawing of our unofficial mascot" className="mascot"/>
+    </div>
+  );
 }
 
-export default Herosection
+export default Herosection;

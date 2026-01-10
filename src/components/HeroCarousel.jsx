e563a9d1012d281bfbb06592a55b44d5d1480679
Cinnie <cinnielin123@gmail.com>
2026-01-10 10:47:10 -0500

    Added Open Sans font to index.html and index.css. Worked more on Teams page.

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -2,21 +2,19 @@ import "./Herocarousel.css";
 
 function HeroCarousel() {
     return (
-        <>
         <div className="heroCarousel">
-            <h1 className="ourTeams">Our Teams</h1>
+            <h1 className="ourTeams font-sans">Our <span className="teams">Teams</span></h1>
             <div className="heroMainBlock">
                 <div className="heroLeftside">
                     <div className="flashcards"></div>
                 </div>
-                <div className="heroRightSide">
+                <div className="heroRightside">
                     <h2>Operations</h2>
-                    <h4>Description goes here</h4>
-                    <button>Learn More</button>
+                    <h4>Description goes here faaaaaaa aaaafdsafdsaf aaaaaaaasweffff fffff ffffffffffffff</h4>
+                    <button className="learnMoreBtn">Learn More</button>
                 </div>
             </div>
         </div>
-        </>
     )
 
 }

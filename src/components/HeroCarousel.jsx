87ed69bb47798ade74e98cc57eef3227389dcad2
Cinnie <cinnielin123@gmail.com>
2026-01-11 19:38:22 -0500

    When hovering over email button, now shows the member's email.

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -10,7 +10,7 @@ function TeamsSection({ backColor, textColor, text, arr }) {
         <div className="teamTitle font-sans" style={{background: backColor}}> 
             {text}
         </div>
-        
+
             <div className="teamMembers">
                 {arr.map((member, index) => (
                     <MemberPhoto
@@ -32,8 +32,10 @@ function MemberPhoto({ name, role, email, color }) {
             <div className="aboveFrame">
                 <h3 style={{ color }}>{role}</h3>
                 <h4 style={{ color }}>{name}</h4>
-                <h5 style={{ color }}>{email}</h5>
-                <img src={Mail} alt="Mail" />
+                <div className="emailBtn">
+                    <img src={Mail} alt="Mail" className="emailImg"/>
+                    <div className="emailLink" style={{ color }}>{email}</div>
+                </div>
             </div>
             
 
@@ -86,49 +88,49 @@ function HeroCarousel() {
                     backColor="linear-gradient(90deg, #AD99FF, #F5FAF7)"
                     textColor="#3111B2"
                     text="E-Board"
-                    arr={[{ name: "Tianyi C.", role: "President", email: "email@email.com" },
-                        { name: "Kyle C.", role: "VP", email: "d@email.com" }, 
-                        { name: "Mikael M-R.", role: "Secretary", email: "email@email.com" }
+                    arr={[{ name: "Tianyi C.", role: "President", email: "tc757@cornell.edu" },
+                        { name: "Kyle C.", role: "VP", email: "ksc224@cornell.edu" }, 
+                        { name: "Mikael M-R.", role: "Secretary", email: "mm3446@cornell.edu" }
                     ]}  
                 />
                 <TeamsSection
                     backColor="linear-gradient(90deg, #F3E880, #F5FAF7)"
                     textColor="#C9AB27"
                     text="Operations"
-                    arr={[{ name: "Jerry C.", role: "Social Media", email: "email2@email.com" }, 
-                        { name: "Kalia C.", role: "Treasurer", email: "email2@email.com" }, 
-                        { name: "Kaavya P.", role: "Business", email: "email2@email.com" }, 
-                        { name: "Aish M.", role: "External Affairs", email: "email2@email.com" }, 
+                    arr={[{ name: "Jerry C.", role: "Social Media", email: "jc3689@cornell.edu" }, 
+                        { name: "Kalia C.", role: "Treasurer", email: "kc2274@cornell.edu" }, 
+                        { name: "Kaavya P.", role: "Business", email: "kp627@cornell.edu" }, 
+                        { name: "Aish M.", role: "External Affairs", email: "am3369@cornell.edu" }, 
                     ]}
                 />
                 <TeamsSection
                     backColor="linear-gradient(90deg, #93C4A6, #F5FAF7)"
                     textColor="#51B976"
                     text="Data Analytics"
-                    arr={[{ name: "Seokyun H.", role: "Lead", email: "email2@email.com" }, 
-                        { name: "Abby C.", role: "Lead", email: "email2@email.com" }, 
-                        { name: "Eric Z.", role: "Member", email: "email2@email.com" }, 
-                        { name: "Minh T.", role: "Member", email: "email2@email.com" }, 
+                    arr={[{ name: "Seokyun H.", role: "Lead", email: "sh2656@cornell.edu" }, 
+                        { name: "Abby C.", role: "Lead", email: "aac265@cornell.edu" }, 
+                        { name: "Eric Z.", role: "Member", email: "ez323@cornell.edu" }, 
+                        { name: "Minh T.", role: "Member", email: "tt536@cornell.edu" }, 
                     ]}
                 />
                 <TeamsSection
                     backColor="linear-gradient(90deg, #FBBEF6, #FAF5F9)"
                     textColor="#EF71E5"
                     text="Engagement and Education"
-                    arr={[{ name: "Tristan A.", role: "Member", email: "email2@email.com" },
-                        { name: "Jimmy Z.", role: "Member", email: "email2@email.com" }
+                    arr={[{ name: "Tristan A.", role: "Member", email: "tja75@cornell.edu" },
+                        { name: "Jimmy Z.", role: "Member", email: "yz3439@cornell.edu" }
                     ]}
                 />
                 <TeamsSection
                     backColor="linear-gradient(90deg, #BED8FB, #F5F8FA)"
                     textColor="#7EB3F8"
                     text="Web Development"
-                    arr={[{ name: "Arsh K.", role: "Lead", email: "email2@email.com" },
-                        { name: "Cinnie L.", role: "Lead", email: "email2@email.com" },
+                    arr={[{ name: "Arsh K.", role: "Lead", email: "ak2676@cornell.edu" },
+                        { name: "Cinnie L.", role: "Lead", email: "cl2728@cornell.edu" },
                         { name: "Winnie C.", role: "Lead", email: "email2@email.com" },
-                        { name: "Amanda Z.", role: "Member", email: "email2@email.com" },
-                        { name: "Anusha K.", role: "Member", email: "email2@email.com" },
-                        { name: "Willow C.", role: "Member", email: "email2@email.com" }
+                        { name: "Amanda Z.", role: "Member", email: "az565@cornell.edu" },
+                        { name: "Anusha K.", role: "Member", email: "wc697@cornell.edu" },
+                        { name: "Willow C.", role: "Member", email: "ark279@cornell.edu" }
                     ]}
                 />
             </div>

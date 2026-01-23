e3a59a9c42272b24ef393294fe3c7b53a9711ffb
Winnie Chan <wchan0806@gmail.com>
2026-01-23 15:25:15 -0500

    Added more to the projects

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,11 +1,42 @@
-import "./ProjectSection.css"
+import "./ProjectSection.css";
 
-function ProjectSection () {
-    return (
-        <div className="projectHero">
-            <h1>Our Projects</h1>
-        </div>
-    )
+function ProjectSection() {
+  const projects = [
+    {
+      title: "ReUse",
+      description:
+        "TCamp is an organizartion that focuses on saving the lives of amphibians during their migration",
+    },
+    {
+      title: "Rubi",
+      description:
+        "TCamp is an organizartion that focuses on saving the lives of amphibians during their migration",
+    },
+    {
+      title: "TCamp",
+      description:
+        "TCamp is an organizartion that focuses on saving the lives of amphibians during their migration",
+    },
+  ];
+
+  return (
+    <div>
+      <div className="projectHero">
+        <h1>Our Projects</h1>
+      </div>
+      <div className="projectContainer centerFlex column">
+        {projects.map((project, index) => {
+          <div key={index} className="centerFlex projectCard">
+            <div className="image-container"></div>
+            <div className="centerFlex column">
+              <h1>{project.title}</h1>
+              <p>{project.description}</p>
+            </div>
+          </div>;
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default ProjectSection; 
+export default ProjectSection;

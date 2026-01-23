78f04794a1abdcf8613806cc4f8c7cec771dfc7a
Winnie Chan <wchan0806@gmail.com>
2026-01-23 14:50:23 -0500

    Initialized the projects page

---
+++ src/components/ProjectSection.jsx
@@ -0,0 +1,11 @@
+import "./ProjectSection.css"
+
+function ProjectSection () {
+    return (
+        <div className="projectHero">
+            <h1>Our Projects</h1>
+        </div>
+    )
+}
+
+export default ProjectSection; 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,12 @@
 import Navigation from "../components/Navigation"
 import Footer from "../components/Footer"
+import ProjectSection from "../components/ProjectSection"
 
 function Projects () {
     return (
         <div>
             <Navigation />
+            <ProjectSection />
             <footer>
                 <Footer />
             </footer>
